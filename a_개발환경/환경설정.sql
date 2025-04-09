71ed9644e0cb24a175f06b0053e508da455817f4
Jangsojeong31 <sojung301@naver.com>
2025-04-09 10:57:07 +0900

    250409 DB 첫수업

---
+++ a_개발환경/환경설정.sql
@@ -0,0 +1,38 @@
+-- 1. 주석
+-- 1) 단축키: ctrl + /
+-- 2) 한 줄 주석: 하이픈 2개 or 샾 1개
+# hello
+-- hello
+
+-- 3) 여러 줄 주석: /* */
+/*
+여러 줄 주석
+*/
+
+-- 이 쿼리는 모든 데이터베이스를 보여줍니다.
+SHOW DATABASES;
+
+-- 2. 글자 크기 변경
+-- ctrl + 마우스 휠
+-- Edit > Preferences > Fonts & Colors > Font명 글자크기값
+
+-- 3. 명령어 대소문자 세팅
+-- 대소문자 구분 x
+-- 일관성 있는 작성 권장
+-- 수업에서는) 명령어는 대문자로, 테이블이나 컬럼명은 소문자로 작성
+-- Edit > Format > UPCASE(대문자) / lowercase(소문자) keywords > 선택 시 자동변경
+show databases; -- 위의 코드와 동일
+
+-- 4. sql 단축키
+# 파일 저장: ctrl + s
+# 한줄 복사: ctrl + d
+# 한줄 삭제: ctrl + l
+# 실행 단축키)
+# 마우스 커서가 위치하는 쿼리문 실행: ctrl + Enter
+# 마우스 드래그 한 영역 실행: ctrl + shift + Enter
+
+-- 5. 다크모드 설정
+-- : Edit > Preferences > Appearance
+
+
+
